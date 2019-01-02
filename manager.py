d34a8a8ba15fdcce2a9d8b7bebe05b285fb6d440
Naor Livne <naor@mintigo.com>
2019-01-02 16:20:09 +0200

    added cache settings to the example config file

--- manager.py
+++ manager.py
@@ -82,7 +82,7 @@ basic_auth_password = get_conf_setting("basic_auth_password", auth_file, None)
 mongo_url = get_conf_setting("mongo_url", auth_file)
 schema_name = get_conf_setting("schema_name", auth_file, "nebula")
 basic_auth_enabled = int(get_conf_setting("basic_auth_enabled", auth_file, True))
-cache_time = int(get_conf_setting("cache_time", auth_file, "60"))
+cache_time = int(get_conf_setting("cache_time", auth_file, "10"))
 cache_max_size = int(get_conf_setting("cache_max_size", auth_file, "1024"))
 
 # login to db at startup
